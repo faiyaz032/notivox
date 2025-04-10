076e6e43442aecd80306ef05acfbdf6d62c1bfd5
faiyaz032 <faiyazrahman03@gmail.com>
2025-04-10 21:53:39 +0600

    refactor(Notivox): update constructor and adapter instantiation logic

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,34 @@ import { ChannelsConfig } from './common/types/ChannelsConfig.type';
 
 export class Notivox {
   private adapters: Map<AdapterNames, AdapterInstances[AdapterNames]> = new Map();
+  private config: ChannelsConfig;
 
-  private constructor() {}
-
-  static async init(config: ChannelsConfig): Promise<Notivox> {
-    const instance = new Notivox();
-    await instance.instantiateAdapters(config);
-    return instance;
+  private constructor(config: ChannelsConfig) {
+    this.config = config;
   }
 
-  getAdapter<T extends AdapterNames>(adapterName: T): AdapterInstances[T] {
-    const adapter = this.adapters.get(adapterName);
-    if (!adapter) {
-      throw new Error(`Adapter ${adapterName} not found`);
-    }
+  static init(config: ChannelsConfig): Notivox {
+    return new Notivox(config);
+  }
+
+  async getAdapter<T extends AdapterNames>(adapterName: T): Promise<AdapterInstances[T]> {
+    let adapter = this.adapters.get(adapterName);
+    if (adapter) return adapter as AdapterInstances[T];
+
+    // Lazy load if not already instantiated
+    await this.instantiateAdapter(adapterName);
+    adapter = this.adapters.get(adapterName);
+
+    if (!adapter) throw new Error(`Adapter ${adapterName} could not be loaded`);
     return adapter as AdapterInstances[T];
   }
 
-  private async instantiateAdapters(config: ChannelsConfig): Promise<void> {
-    // Check for email configuration
-    if (config.email?.nodemailer) {
+  private async instantiateAdapter(adapterName: AdapterNames): Promise<void> {
+    if (adapterName === 'nodemailer' && this.config.email?.nodemailer) {
       const { NodemailerAdapter } = await import('./channels/email/adapters/nodemailer.adapter');
-      // Pass Redis config if available
-      const redisConfig = config.queue?.redis;
-      this.adapters.set('nodemailer', new NodemailerAdapter(config.email.nodemailer, redisConfig));
+      const redisConfig = this.config.queue?.redis;
+      const adapter = new NodemailerAdapter(this.config.email.nodemailer, redisConfig);
+      this.adapters.set('nodemailer', adapter);
     }
   }
 }
